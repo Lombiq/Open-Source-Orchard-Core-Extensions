f24c99e59a226f288c7974a09270273f1ea94ecb
Benedek Farkas <benedek.farkas@lombiq.com>
2019-03-12 15:21:52 +0100

    Adding OSOCE solution generated by Init-OrchardCore

---
+++ src/OSOCE.Web/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using OrchardCore.Logging;
+using OrchardCore.Modules;
+
+namespace OSOCE.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseNLogWeb()
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
