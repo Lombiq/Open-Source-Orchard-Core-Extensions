8c4995b4a8cb4261dfdebd8dea5fe707ba03642a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-09-05 23:45:48 +0200

    Configuring UI Testing in the web app with basic samples

--- src/Lombiq.OSOCE.Web/Startup.cs
+++ src/Lombiq.OSOCE.Web/Startup.cs
@@ -1,18 +1,22 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Lombiq.OSOCE.Web
 {
-    [SuppressMessage(
-        "Major Code Smell",
-        "S1118:Utility classes should not have public constructors",
-        Justification = "Needs to be non-static for UseStartup().")]
     public sealed class Startup
     {
-        public static void ConfigureServices(IServiceCollection services) => services.AddOrchardCms();
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration) => _configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services) =>
+            // Here we're configuring the UI Testing Toolbox (https://github.com/Lombiq/UI-Testing-Toolbox) so UI tests
+            // can be executed on the app. For a tutorial on how to create UI tests check out the project.
+            // ConfigureUITesting() won't do anything when the app is not run for UI testing.
+            services.AddOrchardCms(builder => builder.ConfigureUITesting(_configuration, true));
 
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
