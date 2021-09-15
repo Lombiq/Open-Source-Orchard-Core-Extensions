ebc5e47427e24f06ea928e6eb0ca0932662fbc96
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-09-15 16:24:10 +0200

    Intentional code style violation

--- src/Lombiq.OSOCE.Web/Program.cs
+++ src/Lombiq.OSOCE.Web/Program.cs
@@ -6,7 +6,10 @@ namespace Lombiq.OSOCE.Web
 {
     public static class Program
     {
-        public static void Main(string[] args) => BuildWebHost(args).Run();
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
