32e35069d64769a22f233037ccfff639629a0075
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-09-15 17:19:16 +0200

    Revert "Intentional code style violation"
    This reverts commit ebc5e47427e24f06ea928e6eb0ca0932662fbc96.

--- src/Lombiq.OSOCE.Web/Program.cs
+++ src/Lombiq.OSOCE.Web/Program.cs
@@ -6,10 +6,7 @@ namespace Lombiq.OSOCE.Web
 {
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+        public static void Main(string[] args) => BuildWebHost(args).Run();
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
