c3232766cbf07213033c4fb3967093345fb3f191
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-10-12 19:01:02 +0200

    Tripping the analyzer now for sure

--- src/Lombiq.OSOCE.Web/Startup.cs
+++ src/Lombiq.OSOCE.Web/Startup.cs
@@ -18,7 +18,7 @@ namespace Lombiq.OSOCE.Web
             // ConfigureUITesting() won't do anything when the app is not run for UI testing.
             services.AddOrchardCms(builder => builder.ConfigureUITesting(_configuration, true));
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -28,5 +28,7 @@ namespace Lombiq.OSOCE.Web
             app.UseStaticFiles();
             app.UseOrchardCore();
         }
+
+        private string ShouldViolateAnalyzer() => null;
     }
 }
