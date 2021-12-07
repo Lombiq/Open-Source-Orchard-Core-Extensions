a30ad0ba0953afb1d6444442277e4aad531d3287
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-07 23:20:37 +0100

    Remove unnecessary overrides.

--- src/Modules/Lombiq.OSOCE.Samples/Startup.cs
+++ src/Modules/Lombiq.OSOCE.Samples/Startup.cs
@@ -1,22 +1,8 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
-using System;
 
 namespace Lombiq.OSOCE.Samples
 {
     public class Startup : StartupBase
     {
-        public override void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        public override void Configure(
-            IApplicationBuilder builder,
-            IEndpointRouteBuilder routes,
-            IServiceProvider serviceProvider)
-        {
-        }
     }
 }
