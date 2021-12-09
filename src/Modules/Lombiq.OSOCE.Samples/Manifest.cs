4d25d47d21db52794e68674ab5ef747d73e676db
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-09 20:34:15 +0100

    Fix manifest.

--- src/Modules/Lombiq.OSOCE.Samples/Manifest.cs
+++ src/Modules/Lombiq.OSOCE.Samples/Manifest.cs
@@ -1,9 +1,9 @@
 using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
-    Name = "Lombiq.OSOCE.Samples",
+    Name = "Lombiq Open-Source Orchard Core Extensions - Samples",
     Author = "Lombiq Technologies",
-    Website = "https://lombiq.com",
+    Website = "https://github.com/Lombiq/Open-Source-Orchard-Core-Extensions",
     Version = "1.0.0",
     Description = "Samples for Lombiq Open Source Orchard Core Extensions",
     Category = "Development"
