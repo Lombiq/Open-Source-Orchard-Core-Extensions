4df6fa736d0885b0892b5b46d6b7885ba9a22ddc
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-01-12 02:38:03 +0100

    Updating UI Testing Toolbox to latest, adding temporary controller for troubleshooting

---
+++ src/Modules/Lombiq.OSOCE.Samples/Controllers/TestController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lombiq.OSOCE.Samples.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index() => View();
+    }
+}
