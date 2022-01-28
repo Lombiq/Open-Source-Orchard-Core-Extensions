e670e7d9f37e4f96bc24107903151f07787bf6ee
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-01-28 02:22:42 +0100

    Analyzers and NPM Targets NuGet publishing

--- NuGetTest/src/Modules/Lombiq.OSOCE.NuGet.TestModule/Controllers/SourceLinkTest.cs
+++ NuGetTest/src/Modules/Lombiq.OSOCE.NuGet.TestModule/Controllers/SourceLinkTest.cs
@@ -8,6 +8,9 @@ namespace Lombiq.OSOCE.NuGet.TestModule.Controllers
     {
         private readonly ICachingUserManager _cachingUserManager;
 
+        // Since the Analyzers NuGet package is added, you can see that it works like this: Disable the IDE0021 rule in
+        // the root Analyzers OrchardCore.ruleset file, then change this constructor to use a block body. You should
+        // get an analyzer violation since the expression-bodied rule is still enforced from the package
         public SourceLinkTestController(ICachingUserManager cachingUserManager) => _cachingUserManager = cachingUserManager;
 
         // /Lombiq.OSOCE.NuGet.TestModule/SourceLinkTest/Index
