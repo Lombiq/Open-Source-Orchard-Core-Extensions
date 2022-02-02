6a7b3d6e9991553583194ffb759371df80146846
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-02-02 21:23:06 +0100

    Removing debug code

--- src/Modules/Lombiq.OSOCE.Samples/Controllers/TestController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Lombiq.OSOCE.Samples.Controllers
-{
-    public class TestController : Controller
-    {
-        public ActionResult Index() => View();
-    }
-}
