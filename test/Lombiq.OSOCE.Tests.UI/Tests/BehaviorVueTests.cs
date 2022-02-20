78be5f6d25014dd7f264729bb0f179cd5449e2ad
Dávid El-Saig <david.el-saig@lombiq.com>
2022-02-20 12:35:36 +0100

    Fix test.

--- test/Lombiq.OSOCE.Tests.UI/Tests/BehaviorVueTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/BehaviorVueTests.cs
@@ -1,6 +1,8 @@
 ﻿using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using Lombiq.VueJs.Tests.UI.Extensions;
+using OpenQA.Selenium;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -17,7 +19,14 @@ namespace Lombiq.OSOCE.Tests.UI.Tests
         [Theory, Chrome]
         public Task RecipeDataShouldBeDisplayedCorrectly(Browser browser) =>
             ExecuteTestAfterSetupAsync(
-                context => context.TestVueSampleBehaviorAsync(),
+                async context =>
+                {
+                    await context.SignInDirectlyAndGoToDashboardAsync();
+                    context.ClickReliablyOn(By.Id("privacy-consent-accept-button"));
+                    context.Refresh();
+
+                    await context.TestVueSampleBehaviorAsync();
+                },
                 browser);
     }
 }
