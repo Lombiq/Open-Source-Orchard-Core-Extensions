cfd15e698ee051ebc93d87a0144642d1b2e7a175
Dávid El-Saig <david.el-saig@lombiq.com>
2022-03-13 23:06:41 +0100

    Call clean first.

--- src/Lombiq.OSOCE.Web/Container/bashrc.sh
+++ src/Lombiq.OSOCE.Web/Container/bashrc.sh
@@ -1,2 +1,18 @@
 alias cls=clear
 export XDG_CONFIG_HOME=/home/user/.config
+
+function proxy-nvm-command() {
+    for command in "$@"; do
+
+    cat > ~/.local/bin/$command << DONE
+#!/bin/bash
+
+export NVM_DIR="$HOME/.config/nvm"
+source "\$NVM_DIR/nvm.sh"
+exec $command "\$@"
+DONE
+
+    chmod +x ~/.local/bin/$command
+
+    done
+}
--- src/Lombiq.OSOCE.Web/Container/setup-nvm.sh
+++ src/Lombiq.OSOCE.Web/Container/setup-nvm.sh
@@ -7,5 +7,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source .bashrc
 nvm install 16
 
-npm install -g gulp
-npm install -g pnpm
+proxy-nvm-command node npm
+
+npm install -g gulp pnpm
