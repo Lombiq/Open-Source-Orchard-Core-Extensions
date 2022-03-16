09baa1dd48750e89a3723cc8e52fbdd1be1fca1b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-03-16 21:40:29 +0100

    Wrapping comments and other code styling

--- NuGetTest/src/Modules/Lombiq.OSOCE.NuGet.TestModule/Controllers/SourceLinkTestController.cs
+++ NuGetTest/src/Modules/Lombiq.OSOCE.NuGet.TestModule/Controllers/SourceLinkTestController.cs
@@ -13,10 +13,10 @@ public class SourceLinkTestController : Controller
     // /Lombiq.OSOCE.NuGet.TestModule/SourceLinkTest/Index
     public async Task<string> Index()
     {
-        // If you step into this call from a debugger session you can check if Source Link works and symbols have
-        // been loaded. You should be able to step into the original source. For this to work, you'll need to
-        // untick "Enable Just My Code" in Visual Studio settings under Tools -> Options -> Debugging -> General
-        // and make sure "Enable Source Link support" is checked. For more info see:
+        // If you step into this call from a debugger session you can check if Source Link works and symbols have been
+        // loaded. You should be able to step into the original source. For this to work, you'll need to untick "Enable
+        // Just My Code" in Visual Studio settings under Tools -> Options -> Debugging -> General and make sure "Enable
+        // Source Link support" is checked. For more info see:
         // https://devblogs.microsoft.com/dotnet/improving-debug-time-productivity-with-source-link/#enabling-source-link.
         // Let symbol loading finish, which might take several minutes (though you can speed it up by just loading
         // symbols for "Lombiq.HelpfulLibraries.dll" under Debugging -> Symbols -> Load only specified modules).
