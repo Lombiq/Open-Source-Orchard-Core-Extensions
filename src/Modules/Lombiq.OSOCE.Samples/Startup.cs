34cdd161e2f07566adf14b37ecfba9d3ea34c1a8
Dávid El-Saig <david.el-saig@lombiq.com>
2022-03-27 19:26:52 +0200

    Post-merge code cleanup.

--- src/Modules/Lombiq.OSOCE.Samples/Startup.cs
+++ src/Modules/Lombiq.OSOCE.Samples/Startup.cs
@@ -5,8 +5,8 @@ namespace Lombiq.OSOCE.Samples;
 
 public class Startup : StartupBase
 {
-        public override void ConfigureServices(IServiceCollection services)
-        {
-            // Nothing here yet.
-        }
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        // Nothing here yet.
+    }
 }
