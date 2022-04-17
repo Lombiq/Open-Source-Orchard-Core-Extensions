07ea7d38e2abf27ef701207d83c0eaf7afc91f68
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-18 01:55:24 +0200

    Add tests.

---
+++ test/Lombiq.OSOCE.Tests.UI/Tests/BehaviorThemeTests.cs
@@ -0,0 +1,27 @@
+﻿using Lombiq.BaseTheme.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.Tests.UI.Tests;
+
+public class BehaviorThemeTests : UITestBase
+{
+    public BehaviorThemeTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Theory, Chrome]
+    public Task ThemeFeaturesShouldWork(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            async context =>
+            {
+                await context.SignInDirectlyAndGoToHomepageAsync();
+                await context.TestBaseThemeFeaturesAsync();
+            },
+            browser);
+}
