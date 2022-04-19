91bfc12adf541538f96c37cc208332ba81051608
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-19 14:10:14 +0200

    Fix missing dependency in setup recipe.

--- src/Lombiq.OSOCE.Web/Program.cs
+++ src/Lombiq.OSOCE.Web/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using NLog.Web;
 
@@ -8,11 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseNLog();
 
 var configuration = builder.Configuration;
-
-// Without this we get an error after setup that says "InvalidOperationException: Unable to resolve service for type
-// 'Microsoft.AspNetCore.Identity.IdentityErrorDescriber' while attempting to activate
-// 'Microsoft.AspNetCore.Identity.RoleManager`1[OrchardCore.Security.IRole]'.".
-builder.Services.AddScoped<IdentityErrorDescriber>();
 
 // Here we're configuring the UI Testing Toolbox (https://github.com/Lombiq/UI-Testing-Toolbox) so UI tests can be
 // executed on the app. For a tutorial on how to create UI tests check out the project. ConfigureUITesting() won't do
