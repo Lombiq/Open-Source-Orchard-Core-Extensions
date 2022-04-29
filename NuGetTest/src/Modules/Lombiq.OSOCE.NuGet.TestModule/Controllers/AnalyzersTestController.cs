ea799d52746e59205f07783e98f90a21cd07caad
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-30 00:32:47 +0200

    Update NuGetTest/src/Modules/Lombiq.OSOCE.NuGet.TestModule/Controllers/AnalyzersTestController.cs
    Co-authored-by: 0liver <0liver@users.noreply.github.com>

--- NuGetTest/src/Modules/Lombiq.OSOCE.NuGet.TestModule/Controllers/AnalyzersTestController.cs
+++ NuGetTest/src/Modules/Lombiq.OSOCE.NuGet.TestModule/Controllers/AnalyzersTestController.cs
@@ -8,9 +8,9 @@ public class AnalyzersTestController : Controller
 {
     private readonly IClock _clock;
 
-    // Since the Analyzers NuGet package is added, you can see that it works like this: change this constructor to use a
+    // Since the Analyzers NuGet package is added, you can see that it works like this: Change this constructor to use a
     // block body. You should get an analyzer violation since the expression-bodied rule is still enforced from the
-    // package
+    // package.
     public AnalyzersTestController(IClock clock) => _clock = clock;
 
     // /Lombiq.OSOCE.NuGet.TestModule/AnalyzersTest/Index
