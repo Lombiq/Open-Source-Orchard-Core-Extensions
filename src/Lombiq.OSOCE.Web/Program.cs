55e84360edbb1fd8f607963767bc49c93ada3ef1
Dávid El-Saig <david.el-saig@lombiq.com>
2022-05-03 20:40:40 +0200

    Rename parameter name that hides local variable.

--- src/Lombiq.OSOCE.Web/Program.cs
+++ src/Lombiq.OSOCE.Web/Program.cs
@@ -11,7 +11,8 @@ var configuration = builder.Configuration;
 // Here we're configuring the UI Testing Toolbox (https://github.com/Lombiq/UI-Testing-Toolbox) so UI tests can be
 // executed on the app. For a tutorial on how to create UI tests check out the project. ConfigureUITesting() won't do
 // anything when the app is not run for UI testing.
-builder.Services.AddOrchardCms(builder => builder.ConfigureUITesting(configuration, enableShortcutsDuringUITesting: true));
+builder.Services.AddOrchardCms(orchardCoreBuilder =>
+    orchardCoreBuilder.ConfigureUITesting(configuration, enableShortcutsDuringUITesting: true));
 
 var app = builder.Build();
 
