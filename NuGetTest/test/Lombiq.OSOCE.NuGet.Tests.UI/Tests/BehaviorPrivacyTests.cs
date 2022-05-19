075218320ae89e3fe31c5a4e627a8c7bd5df83e0
József Horváth <info@ministro.hu>
2022-05-19 09:30:21 +0200

    Updating submodules:  - Lombiq.ChartJs: updating version  - Lombiq.Privacy: updating version, add wwwroot  - Lombiq.BaseTheme: updating version  - Lombiq.UITestingToolbox: updating version Adding UI tests and packages to nuget

--- NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/Tests/BehaviorPrivacyTests.cs
+++ NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/Tests/BehaviorPrivacyTests.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Lombiq.OSOCE.Tests.UI.Tests;
+namespace Lombiq.OSOCE.NuGet.Tests.UI.Tests;
 
 public class BehaviorPrivacyTests : UITestBase
 {
