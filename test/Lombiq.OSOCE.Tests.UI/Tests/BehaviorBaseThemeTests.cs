c880087383eadf88c9d86bf2d5e07ca36dfa6549
Dávid El-Saig <david.el-saig@lombiq.com>
2022-05-28 22:43:52 +0200

    Intentional test failures.

--- test/Lombiq.OSOCE.Tests.UI/Tests/BehaviorBaseThemeTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/BehaviorBaseThemeTests.cs
@@ -1,7 +1,9 @@
 ﻿using Lombiq.BaseTheme.Tests.UI.Extensions;
+using Lombiq.Tests.UI;
 using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
+using Microsoft.SqlServer.Management.Dmf;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -24,4 +26,24 @@ public class BehaviorBaseThemeTests : UITestBase
                 await context.TestBaseThemeFeaturesAsync();
             },
             browser);
+
+    [Theory, Chrome]
+    public Task IntentionalTestError1(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            _ => throw new InvalidOperandException("I want to fail."),
+            browser);
+
+    [Theory, Chrome]
+    public Task IntentionalTestError2(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            _ => Throw("Losing is fun."),
+            browser);
+
+    [Theory, Chrome]
+    public Task IntentionalTestError3(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            _ => OrchardCoreUITestBase.Throw(),
+            browser);
+
+    private static void Throw(string message) => throw new InvalidOperandException(message);
 }
