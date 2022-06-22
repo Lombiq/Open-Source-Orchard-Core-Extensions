e7e21920c2124eac1c8b26344439ffbc7c4704e8
Márk Lukovics <mark.lukovics@lombiq.com>
2022-06-22 16:21:07 +0200

    Adding test skeleton

---
+++ test/Lombiq.OSOCE.Tests.UI/Tests/BehaviorUIKitShowcaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Lombiq.ChartJs.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Services;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.Tests.UI.Tests;
+
+public class BehaviorUIKitShowcaseTests : UITestBase
+{
+    public BehaviorUIKitShowcaseTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Theory, Chrome]
+    public Task PlaceholderTestName() { throw new NotImplementedException(); }
+}
