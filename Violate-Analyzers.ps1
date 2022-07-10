76289af74bd58f3d3a783c6936b093b07742a5f0
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-10 19:58:11 +0200

    Do the GitHub PS code analysis from MSBuild as well.

---
+++ Violate-Analyzers.ps1
@@ -0,0 +1,6 @@
+function Violate-Analyzers()
+{
+    "This file is intended made to verify that PSScriptAnalyzer works and contains intentionally bad code."
+}
+
+try { echo Violate-Analyzers } catch { }
