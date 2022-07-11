aaef986d4c0a1470678db3780d216bb780f70051
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-11 02:13:40 +0200

    Remove file used to test analyzer working on GH.

--- Violate-Analyzers.ps1
+++
@@ -1,6 +0,0 @@
-function Violate-Analyzers()
-{
-    "This file is intended made to verify that PSScriptAnalyzer works and contains intentionally bad code."
-}
-
-try { echo Violate-Analyzers } catch { }
