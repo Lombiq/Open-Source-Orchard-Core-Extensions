1d330419bc400771bb6ee3e2668094eed07439c7
Márk Bartha <barthamark@gmail.com>
2022-09-15 18:37:37 +0200

    Adding UI test for media theme

---
+++ test/Lombiq.OSOCE.Tests.UI/Tests/BehaviorMediaThemeTests.cs
@@ -0,0 +1,22 @@
+using Lombiq.Hosting.MediaTheme.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Services;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.Tests.UI.Tests;
+
+public class BehaviorMediaThemeTests : UITestBase
+{
+    public BehaviorMediaThemeTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Theory, Chrome]
+    public Task MediaThemeShouldRenderTemplatesFromMediaLibrary(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            context => context.TestMediaThemeTemplateRenderingBehaviorAsync(),
+            browser);
+}
