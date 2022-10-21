ec40c740633046ebd3c01893f4a817df9e7cb7c8
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-10-21 16:23:38 +0200

    Moving Lombiq.Hosting.Azure.ApplicationInsights UI test logic into test case class

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorApplicationInsightsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorApplicationInsightsTests.cs
@@ -1,7 +1,5 @@
-using Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI.Extensions;
-using Lombiq.Privacy.Tests.UI.Extensions;
+using Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI.TestCases;
 using Lombiq.Tests.UI.Attributes;
-using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using System.Threading.Tasks;
 using Xunit;
@@ -18,15 +16,5 @@ public class BehaviorApplicationInsightsTests : UITestBase
 
     [Theory, Chrome]
     public Task ApplicationInsightsTrackingInOfflineOperationShouldWork(Browser browser) =>
-        ExecuteTestAfterSetupAsync(
-            async context =>
-            {
-                await context.EnableFeatureDirectlyAsync("Lombiq.Hosting.Azure.ApplicationInsights");
-
-                await context.EnablePrivacyConsentBannerFeatureAndAcceptPrivacyConsentAsync();
-
-                context.TestApplicationInsightsTrackingInOfflineOperation();
-            },
-            browser,
-            configuration => configuration.EnableApplicationInsightsOfflineOperation());
+        ApplicationInsightsTestCases.ApplicationInsightsTrackingInOfflineOperationShouldWorkAsync(ExecuteTestAfterSetupAsync, browser);
 }
