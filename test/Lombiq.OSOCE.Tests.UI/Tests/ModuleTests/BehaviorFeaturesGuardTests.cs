7d5dcf33830476fe1e4b335b5e31cfb3ea9ed096
Gábor Pór <gabor.por@lombiq.com>
2022-10-29 16:09:52 +0200

    Re-enabling test

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorFeaturesGuardTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorFeaturesGuardTests.cs
@@ -18,9 +18,7 @@ public class BehaviorFeaturesGuardTests : UITestBase
 
     // HTML validation is disabled as OC's login and dashboard pages contain several errors. See:
     // https://github.com/OrchardCMS/OrchardCore/issues/12271
-#pragma warning disable xUnit1004 // Test methods should not be skipped
-    [Theory(Skip = "Temporarily skipped while FeaturesGuard is disabled."), Chrome]
-#pragma warning restore xUnit1004 // Test methods should not be skipped
+    [Theory, Chrome]
     public Task ForbiddenFeaturesShouldNotBeActivatableOnTenants(Browser browser) =>
         ExecuteTestAfterSetupAsync(
             context => context.TestForbiddenFeaturesAsync(SetupHelpers.RecipeId),
