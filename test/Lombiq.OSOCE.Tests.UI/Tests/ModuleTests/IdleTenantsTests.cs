8ba67499964bb567eda06edf15bb8c4c07764e36
Zoltán Máté <zoltan.mate@lombiq.com>
2022-11-11 21:35:55 +0100

    Not excluding IdleTenantsTests and it should fail

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/IdleTenantsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/IdleTenantsTests.cs
@@ -14,7 +14,7 @@ public class IdleTenantTests : UITestBase
     {
     }
 
-    [Theory(Skip = "Not needed for troubleshooting."), Chrome]
+    [Theory, Chrome]
     public Task ShuttingDownIdleTenantsShouldWork(Browser browser) =>
         ExecuteTestAfterSetupAsync(
             async context =>
