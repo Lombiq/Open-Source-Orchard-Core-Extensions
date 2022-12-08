7ac68cb3a5f1873c9d266b2aae40f2442a566a4a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-12-08 02:12:26 +0100

    Intentionally failing test to check artifact upload

--- NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/Tests/BasicOrchardFeaturesTests.cs
+++ NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/Tests/BasicOrchardFeaturesTests.cs
@@ -1,6 +1,7 @@
 using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -17,6 +18,6 @@ public class BasicOrchardFeaturesTests : UITestBase
     [Theory, Chrome]
     public Task BasicOrchardFeaturesShouldWork(Browser browser) =>
         ExecuteTestAsync(
-            context => context.TestBasicOrchardFeaturesExceptRegistrationAsync("Lombiq.OSOCE.NuGet.BasicOrchardFeaturesTests"),
+            context => context.Exists(By.Id("nothing-here")),
             browser);
 }
