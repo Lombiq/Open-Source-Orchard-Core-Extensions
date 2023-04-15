14f6c6201412da5749a4dafbd4c6d9a1f93ead59
József Horváth <jozsef.horvath@lombiq.com>
2023-04-15 20:00:20 +0200

    Adding UI tests Updating submodule:  - Lombiq.VueJs: Adding UI tests

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorVueTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorVueTests.cs
@@ -1,7 +1,12 @@
-﻿using Lombiq.Tests.UI.Attributes;
+using Atata.HtmlValidation;
+using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
+using Lombiq.VueJs.Samples.Controllers;
 using Lombiq.VueJs.Tests.UI.Extensions;
+using Shouldly;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -24,4 +29,44 @@ public class BehaviorVueTests : UITestBase
                 await context.TestVueSampleBehaviorAsync();
             },
             browser);
+
+    [Theory, Chrome]
+    public Task QrCardScanShouldWorkAsync(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            context => context.TestQrCardFoundAsync(),
+            browser,
+            configuration =>
+            {
+                configuration.BrowserConfiguration.ConfigureFakeVideoSourceForPositiveTest();
+                configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync = AssertHtmValidationResultAsync;
+            });
+
+    [Theory, Chrome]
+    public Task QrCardScanShouldReportNotFoundAsync(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            context => context.TestQrCardNotFoundAsync(),
+            browser,
+            configuration =>
+            {
+                configuration.BrowserConfiguration.ConfigureFakeVideoSourceForNegativeTest();
+                configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync = AssertHtmValidationResultAsync;
+                configuration.AssertBrowserLog = logEntries =>
+                    OrchardCoreUITestExecutorConfiguration.AssertBrowserLogIsEmpty(
+                        logEntries.Where(logEntry =>
+                            // The fetch call reports an error to the browser console if the QrCardController.GetBusinessCard
+                            // results in NotFound.
+                            !(
+                                logEntry.Message.ContainsOrdinalIgnoreCase(nameof(QrCardController.GetBusinessCard))
+                                && logEntry.Message.ContainsOrdinalIgnoreCase(
+                                    "Failed to load resource: the server responded with a status of 404"))));
+            });
+
+    private static async Task AssertHtmValidationResultAsync(HtmlValidationResult validationResult)
+    {
+        var errors = (await validationResult.GetErrorsAsync())
+            .Where(error =>
+                !error.ContainsOrdinalIgnoreCase("The autoplay attribute is not allowed on <video>")
+                && !error.ContainsOrdinalIgnoreCase("title text cannot be longer than 70 characters"));
+        errors.ShouldBeEmpty();
+    }
 }
