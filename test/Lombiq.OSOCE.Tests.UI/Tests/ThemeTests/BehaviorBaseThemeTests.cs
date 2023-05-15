5b62d1ea8989d05bf523201d4cafb4fc403c107f
Dávid El-Saig <david.el-saig@lombiq.com>
2023-05-16 00:38:16 +0200

    add unit test configuration

--- test/Lombiq.OSOCE.Tests.UI/Tests/ThemeTests/BehaviorBaseThemeTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ThemeTests/BehaviorBaseThemeTests.cs
@@ -1,7 +1,9 @@
 ﻿using Lombiq.BaseTheme.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Pages;
 using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -24,4 +26,35 @@ public class BehaviorBaseThemeTests : UITestBase
                 await context.TestBaseThemeFeaturesAsync();
             },
             browser);
+
+    [Theory, Chrome]
+    public Task ThemeWithoutSetupShouldWork(Browser browser) =>
+        ExecuteTestAsync(
+            async context =>
+            {
+                await context.SignInDirectlyAsync();
+            },
+            browser,
+            async context =>
+            {
+                // We explicitly add a custom configuration with the "Blog" setup to test if the feature works when we
+                // add it to an existing site.
+                var homepageUri = await context.GoToSetupPageAndSetupOrchardCoreAsync(
+                    new OrchardCoreSetupParameters(context)
+                    {
+                        SiteName = "Lombiq's OSOCE - UI Testing - With Blog Setup",
+                        RecipeId = "Blog",
+                        TablePrefix = "OSOCE_blog",
+                        SiteTimeZoneValue = "Europe/Budapest",
+                    });
+
+                context.Exists(By.Id("navbar"));
+
+                return homepageUri;
+            },
+            configuration =>
+            {
+                ChangeConfiguration(configuration);
+                return Task.CompletedTask;
+            });
 }
--- test/Lombiq.OSOCE.Tests.UI/UITestBase.cs
+++ test/Lombiq.OSOCE.Tests.UI/UITestBase.cs
@@ -35,15 +35,19 @@ public class UITestBase : OrchardCoreUITestBase<Program>
             setupOperation,
             async configuration =>
             {
-                configuration.BrowserConfiguration.DefaultBrowserSize = CommonDisplayResolutions.HdPlus;
-
-                configuration.BrowserConfiguration.Headless =
-                    TestConfigurationManager.GetBoolConfiguration("BrowserConfiguration:Headless", defaultValue: false);
-
-                configuration.AssertAppLogsAsync = AssertAppLogsHelpers.AssertOsoceAppLogsAreEmptyAsync;
-
+                ChangeConfiguration(configuration);
                 if (changeConfigurationAsync != null) await changeConfigurationAsync(configuration);
             });
+
+    protected void ChangeConfiguration(OrchardCoreUITestExecutorConfiguration configuration)
+    {
+        configuration.BrowserConfiguration.DefaultBrowserSize = CommonDisplayResolutions.HdPlus;
+
+        configuration.BrowserConfiguration.Headless =
+            TestConfigurationManager.GetBoolConfiguration("BrowserConfiguration:Headless", defaultValue: false);
+
+        configuration.AssertAppLogsAsync = AssertAppLogsHelpers.AssertOsoceAppLogsAreEmptyAsync;
+    }
 
     public static readonly Func<IWebApplicationInstance, Task> AssertAppLogsDefaultOSOCEAsync =
         async webApplicationInstance =>
