d0b2ca209ee90db5ae614c411e7889b77f9744e2
Dávid El-Saig <david.el-saig@lombiq.com>
2023-05-19 10:21:37 +0200

    Extract into a different test

---
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ThemeTests/BlogBaseThemeTests.cs
@@ -0,0 +1,37 @@
+﻿using Lombiq.BaseTheme.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.Tests.UI.Tests.ThemeTests;
+
+// Different tests with different setups should not be run on the same set. So this has to be separated from any other
+// Base Theme tests to successfully set up using the Blog recipe.
+public class BlogBehaviorBaseThemeTests : UITestBase
+{
+    public BlogBehaviorBaseThemeTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Theory, Chrome]
+    public Task ThemeWithoutSetupShouldWork(Browser browser) =>
+        ExecuteTestAfterSetupWithBlogRecipeAsync(
+            async context =>
+            {
+                await context.SignInDirectlyAsync();
+                await context.GoToRelativeUrlAsync("/Admin/Themes");
+
+                await context.ClickReliablyOnAsync(By.CssSelector(
+                    "form[action='/Admin/Themes/SetCurrentTheme/Lombiq.BaseTheme.Samples'] button"));
+                context.ShouldBeSuccess();
+
+                await context.GoToHomePageAsync();
+                await context.TestBaseThemeFeaturesAsync(skipLogin: true);
+            },
+            browser);
+}
