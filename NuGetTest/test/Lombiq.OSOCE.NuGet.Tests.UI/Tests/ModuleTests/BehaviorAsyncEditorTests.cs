2359238ac05a307a1691bbb280053e391da78d00
Márk Bartha <barthamark@gmail.com>
2023-06-09 12:27:37 +0200

    Updating test

---
+++ NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/Tests/ModuleTests/BehaviorAsyncEditorTests.cs
@@ -0,0 +1,28 @@
+using Lombiq.ContentEditors.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.NuGet.Tests.UI.Tests.ModuleTests;
+
+public class BehaviorAsyncEditorTests : UITestBase
+{
+    public BehaviorAsyncEditorTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Theory, Chrome]
+    public Task DemoAsyncEditorShouldLoad(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            async context =>
+            {
+                await context.SignInDirectlyAsync();
+                await context.EnableContentEditorsSamplesFeatureAsync();
+                await context.TestDemoAsyncEditorLoadOnAdminAsync();
+            },
+            browser);
+}
