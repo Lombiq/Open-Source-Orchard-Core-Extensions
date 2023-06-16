a01979efa7d130b662eab6e88ae62f323f1b5379
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-16 18:29:47 +0200

    Add UI test.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ThemeTests/BlogBaseThemeTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ThemeTests/BlogBaseThemeTests.cs
@@ -6,6 +6,7 @@ using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
 using Shouldly;
 using System;
+using System.Globalization;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -42,6 +43,49 @@ public class BlogBehaviorBaseThemeTests : UITestBase
                 // Verify the menu items added by the Blog recipe.
                 context.Get(By.CssSelector(".menuWidget__content .nav-link[href='/']")).Text.Trim().ShouldBe("Home");
                 context.Get(By.CssSelector(".menuWidget__content .nav-link[href='/about']")).Text.Trim().ShouldBe("About");
+            },
+            browser);
+
+    [Theory, Chrome]
+    public Task ContentMenuItemShouldWorkCorrectly(Browser browser) =>
+        ExecuteTestAfterSetupAndThemeSwitchAsync(
+            async context =>
+            {
+                await context.SignInDirectlyAsync();
+                await context.GoToRelativeUrlAsync("/Admin/Contents/ContentItems/Menu");
+                await context.ClickReliablyOnAsync(By.ClassName("edit"));
+
+                await context.ClickReliablyOnAsync(By.XPath($"//button[contains(., 'Add Menu Item')]"));
+                await context.ClickReliablyOnAsync(By.XPath("//div[contains(@class, 'card') and .//h4[contains(., 'Content Menu Item')]]//div[contains(@class, 'card-footer')]//a"));
+                await context.ClickAndFillInWithRetriesAsync(By.Id("ContentMenuItemPart_Name"), "My Content");
+
+                // Find content item index by display text and select it from the content picker..
+                context.ExecuteScript(@"
+                    fetch('/Admin/ContentFields/SearchContentItems?part=ContentMenuItemPart&field=SelectedContentItem')
+                        .then((response) => response.json())
+                        .then((json) => json
+                            .map((item, index) => { item.index = index; return item })
+                            .filter((item) => item.displayText === 'Man must explore, and this is exploration at its greatest')[0]
+                            .index)
+                        .then((index) => {
+                            const div = document.createElement('div');
+                            div.id = 'target-index';
+                            div.innerHTML = index;
+                            document.querySelector('.ta-content').appendChild(div);
+                        });");
+                var index = int.Parse(context.Get(By.Id("target-index")).Text.Trim(), CultureInfo.InvariantCulture);
+                await context.SetContentPickerByIndexAsync("ContentMenuItemPart", "SelectedContentItem", index);
+
+                await context.ClickPublishAsync();
+                await context.ClickPublishAsync();
+                context.ShouldBeSuccess();
+
+                await context.GoToHomePageAsync();
+                context
+                    .Get(By.XPath("id('navigation')//li[contains(@class, 'menuWidget__topLevel')]/a[@href='/blog/post-1']"))
+                    .Text
+                    .Trim()
+                    .ShouldBe("My Content");
             },
             browser);
 
