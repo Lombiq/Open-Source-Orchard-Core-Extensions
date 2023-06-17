84680c51f86b4ed78089aa8720541c67e40350e5
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-17 23:01:02 +0200

    Code cleanup.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ThemeTests/BlogBaseThemeTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ThemeTests/BlogBaseThemeTests.cs
@@ -4,7 +4,6 @@ using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
-using Shouldly;
 using System;
 using System.Threading.Tasks;
 using Xunit;
@@ -29,16 +28,13 @@ public class BlogBehaviorBaseThemeTests : UITestBase
             async context =>
             {
                 await context.SignInDirectlyAsync();
-                await context.BaseThemeDependencyShouldBeEnabledAsync();
+                await context.TestBaseThemeDependencyIsEnabledAsync();
 
                 await context.GoToHomePageAsync();
                 await context.TestBaseThemeFeaturesAsync(skipLogin: true);
                 await context.SignInDirectlyAndGoToHomepageAsync();
 
-                // Verify the menu items added by the Blog recipe. This is not in an extension method because it relies
-                // on this specific setup and content.
-                context.Get(By.CssSelector(".menuWidget__content .nav-link[href='/']")).Text.Trim().ShouldBe("Home");
-                context.Get(By.CssSelector(".menuWidget__content .nav-link[href='/about']")).Text.Trim().ShouldBe("About");
+                context.TestBlogRecipeMenuItemsAddedToMainMenu();
             },
             browser);
 
@@ -48,29 +44,7 @@ public class BlogBehaviorBaseThemeTests : UITestBase
             async context =>
             {
                 await context.SignInDirectlyAsync();
-                await context.GoToAdminRelativeUrlAsync("/Contents/ContentItems/Menu");
-                await context.ClickReliablyOnAsync(By.ClassName("edit"));
-
-                await context.ClickReliablyOnAsync(By.XPath("//button[contains(., 'Add Menu Item')]"));
-                await context.ClickReliablyOnAsync(By.XPath(
-                    "//div[contains(@class, 'card') and .//h4[contains(., 'Content Menu Item')]]//div[contains(@class, 'card-footer')]//a"));
-                await context.ClickAndFillInWithRetriesAsync(By.Id("ContentMenuItemPart_Name"), "My Content");
-
-                await context.SetContentPickerByDisplayTextAsync(
-                    "ContentMenuItemPart",
-                    "SelectedContentItem",
-                    "Man must explore, and this is exploration at its greatest");
-
-                await context.ClickPublishAsync();
-                await context.ClickPublishAsync();
-                context.ShouldBeSuccess();
-
-                await context.GoToHomePageAsync();
-                context
-                    .Get(By.XPath("id('navigation')//li[contains(@class, 'menuWidget__topLevel')]/a[@href='/blog/post-1']"))
-                    .Text
-                    .Trim()
-                    .ShouldBe("My Content");
+                await context.TestAddingMenuItemToBlogMainMenu();
             },
             browser);
 
