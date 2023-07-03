ad43d933c80b795f5af56892984870a780eac3b2
Roland Márkus <roland.markus@lombiq.com>
2023-07-03 11:31:07 +0200

    Removing unused directive.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorIdleTenantsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorIdleTenantsTests.cs
@@ -5,7 +5,6 @@ using Lombiq.Tests.UI.Services;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
-using static Lombiq.Hosting.Tenants.IdleTenantManagement.Tests.UI.Constants.IdleTenantData;
 
 namespace Lombiq.OSOCE.Tests.UI.Tests.ModuleTests;
 
