1f42c688c46880669dd72a901a23943c3e8a4caf
Dávid El-Saig <david.el-saig@lombiq.com>
2023-09-10 22:34:56 +0200

    Add visual verification test.

---
+++ test/Lombiq.OSOCE.Tests.UI/Tests/VisualVerificationTests/VisualVerificationTests.cs
@@ -0,0 +1,33 @@
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Constants;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using SixLabors.ImageSharp;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.Tests.UI.Tests.VisualVerificationTests;
+
+public class VisualVerificationTests : UITestBase
+{
+    public static readonly Size[] VisualVerificationSizes =
+    {
+        CommonDisplayResolutions.Fhd,
+    };
+
+    public VisualVerificationTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Theory, Chrome]
+    public Task VerifyHomePageAndLayout(Browser browser) =>
+        // Check the whole page so we can verify the margins and to see if header/footer is affected.
+        ExecuteTestAfterSetupAsync(
+            context => context.AssertVisualVerificationOnAllResolutions(
+                VisualVerificationSizes,
+                _ => By.TagName("body")),
+            browser);
+}
