f396df0ed5486d76d3db32ad9e681758918d1413
József Horváth <jozsef.horvath@lombiq.com>
2023-10-24 00:47:11 +0200

    Reverting changes

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorChartJsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorChartJsTests.cs
@@ -1,13 +1,6 @@
-using Atata.HtmlValidation;
-using Lombiq.ChartJs.Samples.Controllers;
 using Lombiq.ChartJs.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Attributes;
-using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
-using Lombiq.Tests.UI.Services.Counters.Extensions;
-using Shouldly;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -25,28 +18,5 @@ public class BehaviorChartJsTests : UITestBase
     public Task RecipeDataShouldBeDisplayedCorrectly(Browser browser) =>
         ExecuteTestAfterSetupAsync(
             context => context.TestChartJsSampleBehaviorAsync(),
-            browser,
-            configuration =>
-            {
-                configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync = AssertHtmValidationResultAsync;
-                configuration.CounterConfiguration.Running.PhaseThreshold.DbReaderReadThreshold = 70;
-                configuration.CounterConfiguration.Running.ConfigureForRelativeUrl<SampleController>(
-                    configuration => configuration.NavigationThreshold.DbReaderReadThreshold = 51,
-                    controller => controller.Balance(),
-                    exactMatch: true);
-
-                configuration.CounterConfiguration.Running.ConfigureForRelativeUrl<SampleController>(
-                    configuration => configuration.NavigationThreshold.DbReaderReadThreshold = 70,
-                    controller => controller.History(null, null),
-                    exactMatch: true);
-
-                return Task.CompletedTask;
-            });
-
-    private static async Task AssertHtmValidationResultAsync(HtmlValidationResult validationResult)
-    {
-        var errors = (await validationResult.GetErrorsAsync())
-            .Where(error => !error.ContainsOrdinalIgnoreCase("title text cannot be longer than 70 characters"));
-        errors.ShouldBeEmpty();
-    }
+            browser);
 }
--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorDataTablesTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorDataTablesTests.cs
@@ -1,14 +1,6 @@
-using Atata.HtmlValidation;
-using Lombiq.DataTables.Controllers.Api;
-using Lombiq.DataTables.Samples.Controllers;
 using Lombiq.DataTables.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Attributes;
-using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
-using Lombiq.Tests.UI.Services.Counters.Extensions;
-using Shouldly;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -26,37 +18,5 @@ public class BehaviorDataTablesTests : UITestBase
     public Task RecipeDataShouldBeDisplayedCorrectly(Browser browser) =>
         ExecuteTestAfterSetupAsync(
             context => context.TestDataTableRecipeDataAsync(),
-            browser,
-            configuration =>
-            {
-                configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync = AssertHtmValidationResultAsync;
-                configuration.CounterConfiguration.Running.ConfigureForRelativeUrl<SampleController>(
-                    configuration =>
-                    {
-                        configuration.NavigationThreshold.DbReaderReadThreshold = 51;
-                        configuration.PageLoadThreshold.DbReaderReadThreshold = 51;
-                        configuration.SessionThreshold.DbReaderReadThreshold = 51;
-                    },
-                    controller => controller.DataTableTagHelper(),
-                    exactMatch: true);
-                configuration.CounterConfiguration.Running.ConfigureForRelativeUrl<RowsController>(
-                    configuration =>
-                    {
-                        configuration.PageLoadThreshold.DbReaderReadThreshold = 57;
-                        configuration.SessionThreshold.DbReaderReadThreshold = 57;
-                    },
-                    controller => controller.Get(null),
-                    exactMatch: false);
-                configuration.CounterConfiguration.Running.PhaseThreshold.DbReaderReadThreshold = 57;
-                configuration.CounterConfiguration.Running.PhaseThreshold.DbReaderReadThreshold = 171;
-                configuration.CounterConfiguration.Running.PhaseThreshold.DbCommandTextExecutionThreshold = 171;
-                return Task.CompletedTask;
-            });
-
-    private static async Task AssertHtmValidationResultAsync(HtmlValidationResult validationResult)
-    {
-        var errors = (await validationResult.GetErrorsAsync())
-            .Where(error => !error.ContainsOrdinalIgnoreCase("title text cannot be longer than 70 characters"));
-        errors.ShouldBeEmpty();
-    }
+            browser);
 }
