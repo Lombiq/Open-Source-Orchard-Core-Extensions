3a31c6d1b3e8b164d96a9bd24720b7bffb7c15a1
Szabolcs Deme <szabolcs.deme@lombiq.com>
2024-01-18 01:50:13 +0100

    Adding the NuGet version of the Lombiq.Walkthroughs

---
+++ NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
@@ -0,0 +1,32 @@
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Walkthroughs.Tests.UI.Extensions;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.NuGet.Tests.UI.Tests.ModuleTests;
+
+public class BehaviorWalkthroughsTests : UITestBase
+{
+    public BehaviorWalkthroughsTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public Task WalkthroughsShouldWorkCorrectly() =>
+        ExecuteTestAsync(
+            async context =>
+            {
+                await context.GoToSetupPageAndSetupOrchardCoreAsync("Lombiq.Walkthroughs");
+
+                await context.TestWalkthroughsBehaviorAsync();
+            },
+            // Could be removed if https://github.com/shepherd-pro/shepherd/issues/2555 is fixed.
+            changeConfiguration: configuration => configuration.HtmlValidationConfiguration.HtmlValidationOptions =
+                configuration.HtmlValidationConfiguration.HtmlValidationOptions
+                    .CloneWith(validationOptions => validationOptions.ConfigPath =
+                        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BehaviorWalkthroughsTests.htmlvalidate.json")));
+}
