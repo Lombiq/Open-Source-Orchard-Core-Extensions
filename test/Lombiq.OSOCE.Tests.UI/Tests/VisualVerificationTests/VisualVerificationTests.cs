e0239620876faed9954b388ceaa6eed071d2efd6
Roland Márkus <roland.markus@lombiq.com>
2024-01-29 19:58:12 +0100

    Removing directives.

--- test/Lombiq.OSOCE.Tests.UI/Tests/VisualVerificationTests/VisualVerificationTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/VisualVerificationTests/VisualVerificationTests.cs
@@ -3,7 +3,6 @@ using Lombiq.Tests.UI.Extensions;
 using OpenQA.Selenium;
 using SixLabors.ImageSharp;
 using System;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
