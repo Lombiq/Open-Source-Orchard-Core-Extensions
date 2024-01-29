2e6594787a07f6a643189848c96fdbbe4fa25e84
Roland Márkus <roland.markus@lombiq.com>
2024-01-29 12:56:19 +0100

    Fixing test.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorJsonEditorTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorJsonEditorTests.cs
@@ -1,4 +1,7 @@
 using Lombiq.JsonEditor.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -9,5 +12,17 @@ public class BehaviorJsonEditorTests(ITestOutputHelper testOutputHelper) : UITes
 {
     [Fact]
     public Task JsonEditorShouldWorkCorrectly() =>
-        ExecuteTestAfterSetupAsync(context => context.TestJsonEditorBehaviorAsync());
+        ExecuteTestAfterSetupAsync(
+            context =>
+            context.TestJsonEditorBehaviorAsync(),
+            configuration => configuration.AssertBrowserLog = logEntries =>
+            {
+                // This is needed to avoid missing jQuery
+                // Can be removed after this issue is resolved and deployed https://github.com/OrchardCMS/OrchardCore/issues/15181
+                var messageWithoutJqueryError = logEntries.Where(logEntry =>
+                !logEntry.Message.ContainsOrdinalIgnoreCase(
+                    "Uncaught ReferenceError: $ is not defined"));
+
+                OrchardCoreUITestExecutorConfiguration.AssertBrowserLogIsEmpty(messageWithoutJqueryError);
+            });
 }
