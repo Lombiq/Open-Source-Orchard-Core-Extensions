86ba347bc3c07ad306bb8a377d5ed3d6db607b6f
Roland Márkus <roland.markus@lombiq.com>
2024-02-01 20:18:02 +0100

    Back to false.

--- test/Lombiq.OSOCE.Tests.UI/UITestBase.cs
+++ test/Lombiq.OSOCE.Tests.UI/UITestBase.cs
@@ -43,7 +43,7 @@ public abstract class UITestBase : OrchardCoreUITestBase<Program>
         configuration.BrowserConfiguration.DefaultBrowserSize = CommonDisplayResolutions.HdPlus;
 
         configuration.BrowserConfiguration.Headless =
-            TestConfigurationManager.GetBoolConfiguration("BrowserConfiguration:Headless", defaultValue: true);
+            TestConfigurationManager.GetBoolConfiguration("BrowserConfiguration:Headless", defaultValue: false);
 
         configuration.AssertAppLogsAsync = AssertAppLogsHelpers.AssertOsoceAppLogsAreEmptyAsync;
     }
