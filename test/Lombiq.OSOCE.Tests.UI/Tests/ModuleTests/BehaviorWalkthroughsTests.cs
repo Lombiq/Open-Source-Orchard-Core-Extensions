19a6fe94773293aeceb3762b357b714c6e97941b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-11 04:32:21 +0100

    Steps up until import

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
@@ -1,6 +1,11 @@
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
 using Lombiq.Walkthroughs.Tests.UI.Extensions;
+using OpenQA.Selenium;
+using Shouldly;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -19,8 +24,21 @@ public class BehaviorWalkthroughsTests : UITestBase
         ExecuteTestAsync(
             context => context.RunSetupAndTestWalkthroughsBehaviorAsync(),
             // Could be removed if https://github.com/shepherd-pro/shepherd/issues/2555 is fixed.
-            changeConfiguration: configuration => configuration.HtmlValidationConfiguration.HtmlValidationOptions =
-                configuration.HtmlValidationConfiguration.HtmlValidationOptions
-                    .CloneWith(validationOptions => validationOptions.ConfigPath =
-                        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BehaviorWalkthroughsTests.htmlvalidate.json")));
+            changeConfiguration: configuration =>
+            {
+                configuration.HtmlValidationConfiguration.HtmlValidationOptions =
+                    configuration.HtmlValidationConfiguration.HtmlValidationOptions
+                        .CloneWith(validationOptions => validationOptions.ConfigPath =
+                            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BehaviorWalkthroughsTests.htmlvalidate.json"));
+
+                configuration.AssertBrowserLog = logEntries => logEntries.ShouldNotContain(
+                    logEntry => IsValidLogEntry(logEntry),
+                    logEntries.Where(IsValidLogEntry).ToFormattedString());
+            });
+
+    private static bool IsValidLogEntry(LogEntry logEntry) =>
+        OrchardCoreUITestExecutorConfiguration.IsValidBrowserLogEntry(logEntry) &&
+        // See https://github.com/OrchardCMS/OrchardCore/issues/15301.
+        !logEntry.Message.ContainsOrdinalIgnoreCase(
+            "/OrchardCore.Resources/Scripts/jquery.js?v=NZgNvzR7iJazTVAtnyirXhsLcMZkO1V7t4f3DXx0vqE 3128:6 Uncaught");
 }
