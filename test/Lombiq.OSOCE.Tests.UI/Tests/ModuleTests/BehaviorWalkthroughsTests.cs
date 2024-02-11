a7384acfe16746d1b2c31498c92c4053f7353745
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-11 20:21:27 +0100

    Completing all Walkthroughs test steps

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
@@ -40,5 +40,8 @@ public class BehaviorWalkthroughsTests : UITestBase
         OrchardCoreUITestExecutorConfiguration.IsValidBrowserLogEntry(logEntry) &&
         // See https://github.com/OrchardCMS/OrchardCore/issues/15301.
         !logEntry.Message.ContainsOrdinalIgnoreCase(
-            "/OrchardCore.Resources/Scripts/jquery.js?v=NZgNvzR7iJazTVAtnyirXhsLcMZkO1V7t4f3DXx0vqE 3128:6 Uncaught");
+            "/OrchardCore.Resources/Scripts/jquery.js?v=NZgNvzR7iJazTVAtnyirXhsLcMZkO1V7t4f3DXx0vqE 3128:6 Uncaught") &&
+        // See https://github.com/OrchardCMS/OrchardCore/issues/14598. This error has multiple variations, so targeting
+        // the lower common denominator with the file name.
+        !logEntry.Message.ContainsOrdinalIgnoreCase("/monaco/IStandaloneEditorConstructionOptions.json");
 }
