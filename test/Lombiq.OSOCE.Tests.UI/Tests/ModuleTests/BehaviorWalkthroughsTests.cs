897cf74d6cd4319448b9021c95cabd1cb95a11a6
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-12 18:11:00 +0100

    Revert "Naming the test itself so it's at the beginning alphabetically"
    This reverts commit 462325e6cc3cafbecf5b4bd5daf8e308d7afc597.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
@@ -12,15 +12,15 @@ using Xunit.Abstractions;
 
 namespace Lombiq.OSOCE.Tests.UI.Tests.ModuleTests;
 
-public class BehaviorWalkthroughsTests : UITestBase
+public class ABehaviorWalkthroughsTests : UITestBase
 {
-    public BehaviorWalkthroughsTests(ITestOutputHelper testOutputHelper)
+    public ABehaviorWalkthroughsTests(ITestOutputHelper testOutputHelper)
         : base(testOutputHelper)
     {
     }
 
     [Fact]
-    public Task AllWalkthroughsShouldWorkCorrectly() =>
+    public Task WalkthroughsShouldWorkCorrectly() =>
         ExecuteTestAsync(
             context => context.RunSetupAndTestWalkthroughsBehaviorAsync(),
             // Could be removed if https://github.com/shepherd-pro/shepherd/issues/2555 is fixed.
