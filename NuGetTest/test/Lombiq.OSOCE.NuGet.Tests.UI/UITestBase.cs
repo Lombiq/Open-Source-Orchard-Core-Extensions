38fe2832509408ce5df4d82428dc2d9e336b930a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-20 15:13:11 +0100

    Formatting, small fixes

--- NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/UITestBase.cs
+++ NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/UITestBase.cs
@@ -32,7 +32,8 @@ public class UITestBase : OrchardCoreUITestBase<Program>
             async configuration =>
             {
                 configuration.AssertAppLogsAsync = AssertAppLogsHelpers.AssertOsoceAppLogsAreEmptyAsync;
-                // Can be removed once  https://github.com/OrchardCMS/OrchardCore/issues/15222 is done.
+
+                // These two can be removed once  https://github.com/OrchardCMS/OrchardCore/issues/15222 is done.
                 configuration.AssertBrowserLog = AssertHtmlAndBrowserErrorsHelper.AssertBrowserLogIsEmpty;
                 configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
                     AssertHtmlAndBrowserErrorsHelper.AssertHtmlErrorsAreEmpty;
