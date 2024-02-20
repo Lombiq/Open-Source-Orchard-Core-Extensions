07a0a6f5e16036a16754634b031b063377a8b735
Roland Márkus <roland.markus@lombiq.com>
2024-02-20 18:26:56 +0100

    Fixing typo.

--- NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/Tests/ModuleTests/BehaviorDataTableTests.cs
+++ NuGetTest/test/Lombiq.OSOCE.NuGet.Tests.UI/Tests/ModuleTests/BehaviorDataTableTests.cs
@@ -14,5 +14,5 @@ public class BehaviorDataTableTests : UITestBase
 
     [Fact]
     public Task DataTableShouldWork() => ExecuteTestAfterSetupAsync(
-        tcontext => context.TestDataTableRecipeDataAsync(isNugetTest: true));
+        context => context.TestDataTableRecipeDataAsync(isNugetTest: true));
 }
