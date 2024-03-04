5992e680b4a75f0bc709a5f430e3582d16a1f808
Sára El-Saig <sara.el-saig@lombiq.com>
2024-03-04 23:56:30 +0100

    use ExecuteJsonRecipeSiteSettingAsync

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorEmailQuotaTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorEmailQuotaTests.cs
@@ -1,4 +1,5 @@
 using Lombiq.Hosting.Tenants.EmailQuotaManagement.Tests.UI.Extensions;
+using OrchardCore.Email;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -15,14 +16,19 @@ public class BehaviorEmailQuotaTests : UITestBase
     [Fact]
     public Task EmailQuotaShouldBlockEmailsOverLimitAndWarn() =>
         ExecuteTestAfterSetupAsync(
-            context => context.TestEmailQuotaManagementBehaviorAsync(10),
+            context => context.TestEmailQuotaManagementBehaviorAsync(
+                maximumEmailQuota: 10,
+                new SmtpSettings { Host = "localhost", IsEnabled = true }),
             configuration => configuration.SetEmailQuotaManagementOptionsForUITest(10));
 
     [Fact]
     public Task EmailQuotaShouldNotBlockEmailsWhenDifferentHostIsUsedThanOriginalFromConfig() =>
+        // The default SMTP host is localhost during UI tests, we set it to 127.0.0.1 to be able to send emails, but the
+        // Email Quota module shouldn't interfere.
         ExecuteTestAfterSetupAsync(
-            context => context.TestEmailQuotaManagementBehaviorAsync(1, moduleShouldInterfere: false),
-            // The default SMTP host is localhost during UI tests, we set it to 127.0.0.1 to be able to send emails,
-            // but the Email Quota module shouldn't interfere.
-            configuration => configuration.SetEmailQuotaManagementOptionsForUITest(1, "127.0.0.1"));
+            context => context.TestEmailQuotaManagementBehaviorAsync(
+                maximumEmailQuota: 1,
+                new SmtpSettings { Host = "127.0.0.1", IsEnabled = true },
+                moduleShouldInterfere: false),
+            configuration => configuration.SetEmailQuotaManagementOptionsForUITest(1));
 }
