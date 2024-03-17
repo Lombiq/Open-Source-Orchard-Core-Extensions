b856986245efb073266fabd80b59557516cbb734
Sára El-Saig <sara.el-saig@lombiq.com>
2024-03-18 00:02:03 +0100

    Temporary hack to verify the solution for https://github.com/OrchardCMS/OrchardCore/issues/15533.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorAsyncEditorTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorAsyncEditorTests.cs
@@ -1,6 +1,8 @@
 using Lombiq.ContentEditors.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Helpers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -12,6 +14,10 @@ public class BehaviorAsyncEditorTests : UITestBase
     public BehaviorAsyncEditorTests(ITestOutputHelper testOutputHelper)
         : base(testOutputHelper)
     {
+        // Temporary bug fix until https://github.com/OrchardCMS/OrchardCore/issues/15533 is resolved. Here we edit
+        // Default but there Base has to be updated.
+        JOptions.Default.PreferredObjectCreationHandling = JsonObjectCreationHandling.Populate;
+        JOptions.Default.ReferenceHandler = null;
     }
 
     [Fact]
