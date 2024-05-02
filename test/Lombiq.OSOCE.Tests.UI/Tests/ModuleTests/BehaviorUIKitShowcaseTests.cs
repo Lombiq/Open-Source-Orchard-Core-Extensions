1098a5dddeca769ec656230016612231a8ed3cb1
Aydin Erdas <info@aydinerdas.com>
2024-05-02 10:46:35 +0200

    Update BehaviorUIKitShowcaseTests.cs

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitShowcaseTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitShowcaseTests.cs
@@ -4,7 +4,6 @@ using Lombiq.UIKit.Tests.UI.Extensions;
 using Shouldly;
 using System.Linq;
 using System.Threading.Tasks;
-using Xunit;
 using Xunit.Abstractions;
 
 namespace Lombiq.OSOCE.Tests.UI.Tests.ModuleTests;
@@ -16,26 +15,26 @@ public class BehaviorUIKitShowcaseTests : UITestBase
     {
     }
 
-    //[Fact]
-    //public Task UIKitShowcasePageShouldBeCorrect()
-    //    => ExecuteTestAfterSetupAsync(
-    //        context => context.TestUIKitShowcaseBehaviorAsync(),
-    //        configuration =>
-    //        {
-    //            configuration.HtmlValidationConfiguration.HtmlValidationOptions.OutputFormatter =
-    //                HtmlValidateFormatter.Names.Json;
-    //            configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
-    //                validationResult =>
-    //                {
-    //                    // Error filtering due to https://github.com/OrchardCMS/OrchardCore/issues/15222,
-    //                    // can be removed once it is resolved.
-    //                    var errors = validationResult.GetParsedErrors()
-    //                        .Where(error =>
-    //                            error.RuleId is not "prefer-native-element" and
-    //                                not "text-content" and
-    //                                not "no-redundant-role");
-    //                    errors.ShouldBeEmpty(string.Join('\n', errors.Select(error => error.Message)));
-    //                    return Task.CompletedTask;
-    //                };
-    //        });
+    // [Fact]
+    public Task UIKitShowcasePageShouldBeCorrect()
+        => ExecuteTestAfterSetupAsync(
+            context => context.TestUIKitShowcaseBehaviorAsync(),
+            configuration =>
+            {
+                configuration.HtmlValidationConfiguration.HtmlValidationOptions.OutputFormatter =
+                    HtmlValidateFormatter.Names.Json;
+                configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
+                    validationResult =>
+                    {
+                        // Error filtering due to https://github.com/OrchardCMS/OrchardCore/issues/15222,
+                        // can be removed once it is resolved.
+                        var errors = validationResult.GetParsedErrors()
+                            .Where(error =>
+                                error.RuleId is not "prefer-native-element" and
+                                    not "text-content" and
+                                    not "no-redundant-role");
+                        errors.ShouldBeEmpty(string.Join('\n', errors.Select(error => error.Message)));
+                        return Task.CompletedTask;
+                    };
+            });
 }
