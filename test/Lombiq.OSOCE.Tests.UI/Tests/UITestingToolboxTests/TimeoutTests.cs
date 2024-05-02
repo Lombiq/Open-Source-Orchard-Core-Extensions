504848e592886d64430a444d5b85ee91715f0247
Sára El-Saig <sara.el-saig@lombiq.com>
2024-05-02 09:57:45 +0200

    Add test

---
+++ test/Lombiq.OSOCE.Tests.UI/Tests/UITestingToolboxTests/TimeoutTests.cs
@@ -0,0 +1,18 @@
+using Lombiq.Tests.UI.Tests.UI.TestCases;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.Tests.UI.Tests.UITestingToolboxTests;
+
+public class TimeoutTests : UITestBase
+{
+    public TimeoutTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public Task GenerateHttpEventUrlShouldWork() =>
+        TimeoutTestCases.TestRunTimoutShouldThrowAsync(ExecuteTestAfterSetupAsync);
+}
