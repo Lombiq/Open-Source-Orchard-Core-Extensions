c23af611c3b431dda9ed9be9951632feb5707e9c
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-05-07 23:26:52 +0200

    Temporarily disabling ApplicationInsights

--- src/Lombiq.OSOCE.Web/Program.cs
+++ src/Lombiq.OSOCE.Web/Program.cs
@@ -18,7 +18,6 @@ var configuration = builder.Configuration;
 builder.Services
     .AddSingleton(configuration)
     .AddOrchardCms(orchardCoreBuilder => orchardCoreBuilder
-        .AddOrchardCoreApplicationInsightsTelemetry(configuration)
         .ConfigureFeaturesGuard(
             new Dictionary<string, IEnumerable<string>>
             {
--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorApplicationInsightsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorApplicationInsightsTests.cs
@@ -12,7 +12,9 @@ public class BehaviorApplicationInsightsTests : UITestBase
     {
     }
 
-    [Fact]
+#pragma warning disable xUnit1004 // Test methods should not be skipped
+    [Fact(Skip = "Debugging test hangs.")]
+#pragma warning restore xUnit1004 // Test methods should not be skipped
     public Task ApplicationInsightsTrackingInOfflineOperationShouldWork() =>
         ApplicationInsightsTestCases.ApplicationInsightsTrackingInOfflineOperationShouldWorkAsync(ExecuteTestAfterSetupAsync);
 }
