c5f34f3151646ed60a3cf4933f12f6290a4d9f88
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-05-12 21:56:42 +0200

    Formatting

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitShowcaseTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitShowcaseTests.cs
@@ -20,18 +20,18 @@ public class BehaviorUIKitShowcaseTests : UITestBase
         => ExecuteTestAfterSetupAsync(
             context => context.TestUIKitShowcaseBehaviorAsync(),
             configuration => configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
-                    validationResult =>
-                    {
-                        configuration.HtmlValidationConfiguration.WithRelativeConfigPath("NoUniqueLandmark.htmlvalidate.json");
+                validationResult =>
+                {
+                    configuration.HtmlValidationConfiguration.WithRelativeConfigPath("NoUniqueLandmark.htmlvalidate.json");
 
-                        // Error filtering due to https://github.com/OrchardCMS/OrchardCore/issues/15222,
-                        // can be removed once it is resolved.
-                        var errors = validationResult.GetParsedErrors()
-                            .Where(error =>
-                                error.RuleId is not "prefer-native-element" and
-                                    not "text-content" and
-                                    not "no-redundant-role");
-                        errors.ShouldBeEmpty(HtmlValidationResultExtensions.GetParsedErrorMessageString(errors));
-                        return Task.CompletedTask;
-                    });
+                    // Error filtering due to https://github.com/OrchardCMS/OrchardCore/issues/15222, can be removed
+                    // once it is resolved.
+                    var errors = validationResult.GetParsedErrors()
+                        .Where(error =>
+                            error.RuleId is not "prefer-native-element" and
+                                not "text-content" and
+                                not "no-redundant-role");
+                    errors.ShouldBeEmpty(HtmlValidationResultExtensions.GetParsedErrorMessageString(errors));
+                    return Task.CompletedTask;
+                });
 }
