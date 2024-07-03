a689b7a60cde2e54684c037d48e66d5e839ccf0d
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-07-03 20:13:06 +0200

    Reapply "Intentional PS analyzer violation"
    This reverts commit d8cca42a502edcf18e5a63c61038b3b32f3c0bfe.

--- Reset-Local.ps1
+++ Reset-Local.ps1
@@ -1,1 +1,1 @@
-Remove-Item '.\src\Lombiq.OSOCE.Web\App_Data' -Recurse && Write-Output 'Successfully reset website.'
+Remove-Item '.\src\Lombiq.OSOCE.Web\App_Data' -Recurse && Write-Output 'Successfully reset website.' 
