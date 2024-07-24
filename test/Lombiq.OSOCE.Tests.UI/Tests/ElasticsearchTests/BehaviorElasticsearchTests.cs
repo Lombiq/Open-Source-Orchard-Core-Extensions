3ae4d4f87295d71c9c9d4b852b2f67f821a2cfdd
Gábor Pór <51411356+porgabi@users.noreply.github.com>
2024-07-24 17:45:32 +0200

    Using extension method
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- test/Lombiq.OSOCE.Tests.UI/Tests/ElasticsearchTests/BehaviorElasticsearchTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ElasticsearchTests/BehaviorElasticsearchTests.cs
@@ -19,8 +19,7 @@ public class BehaviorElasticsearchTests : UITestBase
         ExecuteTestAfterSetupAsync(
             async context =>
             {
-                await context.SignInDirectlyAsync();
-                await context.GoToRelativeUrlAsync("/search");
+                await context.SignInDirectlyAndGoToRelativeUrlAsync("/search");
 
                 await context.ClickAndFillInWithRetriesAsync(By.Name("Terms"), "man");
                 await context.ClickReliablyOnAsync(By.XPath("//button[@class='btn btn-primary btn-sm']"));
