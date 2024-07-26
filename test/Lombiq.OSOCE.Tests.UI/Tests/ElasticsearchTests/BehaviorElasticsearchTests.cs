32ddcc1c6fc2a8042958fdecd69e1221fc32764a
Gábor Pór <gabor.por@lombiq.com>
2024-07-26 16:36:58 +0200

    Adding separate test recipe for Elasticsearch test

--- test/Lombiq.OSOCE.Tests.UI/Tests/ElasticsearchTests/BehaviorElasticsearchTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ElasticsearchTests/BehaviorElasticsearchTests.cs
@@ -1,6 +1,10 @@
+using Atata;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Helpers;
+using Lombiq.Tests.UI.Pages;
 using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -16,7 +20,7 @@ public class BehaviorElasticsearchTests : UITestBase
 
     [Fact]
     public Task ElasticsearchSearchingShouldWork() =>
-        ExecuteTestAfterSetupAsync(
+        ExecuteTestAsync(
             async context =>
             {
                 await context.SignInDirectlyAndGoToRelativeUrlAsync("/search");
@@ -26,5 +30,32 @@ public class BehaviorElasticsearchTests : UITestBase
 
                 context.Exists(By.XPath("//h2[contains(., 'Man must explore, and this is exploration at its greatest')]"));
             },
-            ConfigurationHelper.DisableHtmlValidation);
+            setupOperation: async context =>
+            {
+                var homepageUri = await context.GoToSetupPageAndSetupOrchardCoreAsync(
+                    new OrchardCoreSetupParameters(context)
+                    {
+                        SiteName = "Lombiq's OSOCE - UI Testing - Elasticsearch",
+                        RecipeId = "Lombiq.OSOCE.Tests.Elasticsearch",
+                        TablePrefix = "OSOCE",
+                        SiteTimeZoneValue = "Europe/Budapest",
+                    });
+
+                try
+                {
+                    context.Exists(By.Id("navbar"));
+                }
+                catch (NoSuchElementException)
+                {
+                    var validationErrors = context.GetAll(By.ClassName("field-validation-error"));
+
+                    if (validationErrors.Count == 0) throw;
+
+                    var errors = "\n- " + validationErrors.Select(element => element.Text.Trim()).Join("\n- ");
+                    throw new AssertionException($"Setup has failed with the following validation errors:{errors}");
+                }
+
+                return homepageUri;
+            },
+            changeConfigurationAsync: ConfigurationHelper.DisableHtmlValidation);
 }
