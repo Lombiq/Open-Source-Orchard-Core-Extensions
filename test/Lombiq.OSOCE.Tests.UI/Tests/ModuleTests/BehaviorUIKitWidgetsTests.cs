a08ef16d3b11ccf96652edc906a81f4f063cae4c
Miloš Jovanović <milosjovanovic042@gmail.com>
2024-12-20 19:17:03 +0100

    Adding Lombiq UI Kit Widgets behaviour tests.

---
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitWidgetsTests.cs
@@ -0,0 +1,37 @@
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.UIKit.Widgets.Tests.UI.Extensions;
+using Shouldly;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lombiq.OSOCE.Tests.UI.Tests.ModuleTests;
+
+public class BehaviorUIKitWidgetsTests : UITestBase
+{
+    public BehaviorUIKitWidgetsTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public Task UIKitCarouselShouldHaveSlickContainer()
+        => ExecuteTestAfterSetupAsync(
+            context => context.TestUIKitWidgetsBehaviorAsync(),
+            configuration => configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
+                validationResult =>
+                {
+                    configuration.HtmlValidationConfiguration.WithRelativeConfigPath("NoUniqueLandmark.htmlvalidate.json");
+
+                    // Error filtering due to https://github.com/OrchardCMS/OrchardCore/issues/15222, can be removed
+                    // once it is resolved.
+                    var errors = validationResult.GetParsedErrors()
+                        .Where(error =>
+                            error.RuleId is not "prefer-native-element" and
+                                not "text-content" and
+                                not "no-redundant-role");
+                    //errors.ShouldBeEmpty(HtmlValidationResultExtensions.GetParsedErrorMessageString(errors));
+                    return Task.CompletedTask;
+                });
+}
