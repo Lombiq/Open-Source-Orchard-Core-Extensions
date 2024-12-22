8e56b8ae760ed2b1f13c1d991a60e2b1c94f6a71
Miloš Jovanović <milosjovanovic042@gmail.com>
2024-12-22 22:15:07 +0100

    Adding options test for CarouselWidget part; renaming case methods so they have more meaningful names.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitWidgetsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitWidgetsTests.cs
@@ -16,9 +16,29 @@ public class BehaviorUIKitWidgetsTests : UITestBase
     }
 
     [Fact]
-    public Task UIKitCarouselShouldHaveSlickContainer()
+    public Task UIKitCarouselWidgetShouldHaveSlickContainer()
         => ExecuteTestAfterSetupAsync(
-            context => context.TestUIKitWidgetsBehavior(),
+            context => context.TestCarouselWidgetBehavior(),
+            configuration => configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
+                validationResult =>
+                {
+                    configuration.HtmlValidationConfiguration.WithRelativeConfigPath("NoUniqueLandmark.htmlvalidate.json");
+
+                    // Error filtering due to https://github.com/OrchardCMS/OrchardCore/issues/15222, can be removed
+                    // once it is resolved.
+                    var errors = validationResult.GetParsedErrors()
+                        .Where(error =>
+                            error.RuleId is not "prefer-native-element" and
+                                not "text-content" and
+                                not "no-redundant-role");
+                    errors.ShouldBeEmpty(HtmlValidationResultExtensions.GetParsedErrorMessageString(errors));
+                    return Task.CompletedTask;
+                });
+
+    [Fact]
+    public Task CarouselWidgetPartSettingsHasJsonEditorForOptionsAndOptionsAreUsed()
+        => ExecuteTestAfterSetupAsync(
+            context => context.TestCarouselWidgetOptionsAsync(),
             configuration => configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
                 validationResult =>
                 {
