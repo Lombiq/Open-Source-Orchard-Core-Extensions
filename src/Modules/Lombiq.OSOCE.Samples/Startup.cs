3a1a788d3aea673efd5601d007801104e2c9bf5c
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-12 00:38:49 +0200

    Move navigation to UIKit/Showcase.

--- src/Modules/Lombiq.OSOCE.Samples/Startup.cs
+++ src/Modules/Lombiq.OSOCE.Samples/Startup.cs
@@ -1,11 +1,7 @@
-using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
-using OrchardCore.Navigation;
 
 namespace Lombiq.OSOCE.Samples;
 
 public sealed class Startup : StartupBase
 {
-    public override void ConfigureServices(IServiceCollection services) =>
-        services.AddNavigationProvider<Navigation>();
 }
