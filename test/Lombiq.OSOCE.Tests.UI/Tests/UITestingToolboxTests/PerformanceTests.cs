2f3e25a31eceb8646cf32dace34d136930d4364d
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-06-27 20:04:19 +0200

    Selenium.WebDriver v4.31.0, no accessibility checks or HTML validation perf test

--- test/Lombiq.OSOCE.Tests.UI/Tests/UITestingToolboxTests/PerformanceTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/UITestingToolboxTests/PerformanceTests.cs
@@ -1,4 +1,6 @@
 using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -18,10 +20,116 @@ public class PerformanceTests : UITestBase
             {
                 for (int i = 0; i < 99; i++)
                 {
-                    await context.GoToRelativeUrlAsync("/blog/post-1");
-                    await context.GoToRelativeUrlAsync("/about");
-                    await context.GoToHomePageAsync();
+                    if (false)
+                    {
+                        await context.Driver.Navigate().GoToUrlAsync(new Uri(context.Scope.BaseUri, "blog/post-1"));
+                        await context.Driver.Navigate().GoToUrlAsync(new Uri(context.Scope.BaseUri, "about"));
+                        await context.Driver.Navigate().GoToUrlAsync(context.Scope.BaseUri);
+                    }
+
+                    if (false)
+                    {
+                        await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                            () => context.Driver.Navigate().GoToUrlAsync(new Uri(context.Scope.BaseUri, "blog/post-1")));
+
+                        await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                            () => context.Driver.Navigate().GoToUrlAsync(new Uri(context.Scope.BaseUri, "about")));
+
+                        await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                            () => context.Driver.Navigate().GoToUrlAsync(context.Scope.BaseUri));
+                    }
+
+                    if (false)
+                    {
+                        await context.ExecuteLoggedAsync(
+                            "GoToUrlAsync",
+                            "blog/post-1",
+                            async () =>
+                            {
+                                await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                                    () => context.Driver.Navigate().GoToUrlAsync(new Uri(context.Scope.BaseUri, "blog/post-1")));
+                            });
+
+                        await context.ExecuteLoggedAsync(
+                            "GoToUrlAsync",
+                            "about",
+                            async () =>
+                            {
+                                await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                                    () => context.Driver.Navigate().GoToUrlAsync(new Uri(context.Scope.BaseUri, "about")));
+                            });
+
+                        await context.ExecuteLoggedAsync(
+                            "GoToUrlAsync",
+                            "/",
+                            async () =>
+                            {
+                                await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                                    () => context.Driver.Navigate().GoToUrlAsync(context.Scope.BaseUri));
+                            });
+                    }
+
+                    if (false)
+                    {
+                        await context.ExecuteLoggedAsync(
+                            "GoToUrlAsync",
+                            "blog/post-1",
+                            async () =>
+                            {
+                                await context.Configuration.Events.BeforeNavigation
+                                    .InvokeAsync<NavigationEventHandler>(eventHandler => eventHandler(context, new Uri(context.Scope.BaseUri, "blog/post-1")));
+
+                                await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                                    () => context.Driver.Navigate().GoToUrlAsync(new Uri(context.Scope.BaseUri, "blog/post-1")));
+
+                                await context.Configuration.Events.AfterNavigation
+                                    .InvokeAsync<NavigationEventHandler>(eventHandler => eventHandler(context, new Uri(context.Scope.BaseUri, "blog/post-1")));
+                            });
+
+                        await context.ExecuteLoggedAsync(
+                            "GoToUrlAsync",
+                            "about",
+                            async () =>
+                            {
+                                await context.Configuration.Events.BeforeNavigation
+                                    .InvokeAsync<NavigationEventHandler>(eventHandler => eventHandler(context, new Uri(context.Scope.BaseUri, "about")));
+
+                                await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                                    () => context.Driver.Navigate().GoToUrlAsync(new Uri(context.Scope.BaseUri, "about")));
+
+                                await context.Configuration.Events.AfterNavigation
+                                    .InvokeAsync<NavigationEventHandler>(eventHandler => eventHandler(context, new Uri(context.Scope.BaseUri, "about")));
+                            });
+
+                        await context.ExecuteLoggedAsync(
+                            "GoToUrlAsync",
+                            "/",
+                            async () =>
+                            {
+                                await context.Configuration.Events.BeforeNavigation
+                                    .InvokeAsync<NavigationEventHandler>(eventHandler => eventHandler(context, context.Scope.BaseUri));
+
+                                await context.DoWithRetriesUntilNavigationHasOccurredOrFailAsync(
+                                    () => context.Driver.Navigate().GoToUrlAsync(context.Scope.BaseUri));
+
+                                await context.Configuration.Events.AfterNavigation
+                                    .InvokeAsync<NavigationEventHandler>(eventHandler => eventHandler(context, context.Scope.BaseUri));
+                            });
+                    }
+
+                    if (true)
+                    {
+                        await context.GoToRelativeUrlAsync("/blog/post-1");
+                        await context.GoToRelativeUrlAsync("/about");
+                        await context.GoToHomePageAsync();
+                    }
+
                     _testOutputHelper.WriteLine($"Iteration {i + 1} completed.");
                 }
+            },
+            configuration =>
+            {
+                configuration.AccessibilityCheckingConfiguration.RunAccessibilityCheckingAssertionOnAllPageChanges = false;
+                configuration.HtmlValidationConfiguration.RunHtmlValidationAssertionOnAllPageChanges = false;
             });
 }
