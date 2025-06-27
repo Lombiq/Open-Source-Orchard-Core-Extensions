1cc70a7b6a28156d73013195a4e4c54cf0b98c6c
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-06-28 00:17:31 +0200

    Copying the Selenium log too

--- test/Lombiq.OSOCE.Tests.UI/Tests/UITestingToolboxTests/PerformanceTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/UITestingToolboxTests/PerformanceTests.cs
@@ -1,3 +1,5 @@
+using Lombiq.Tests.UI.Extensions;
+using OpenQA.Selenium.Internal.Logging;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -20,7 +22,12 @@ public class PerformanceTests : UITestBase
                 var driver = context.Driver;
                 var baseUri = context.Scope.BaseUri;
 
-                for (int i = 0; i < 3; i++)
+                Log.SetLevel(LogEventLevel.Trace);
+                Log.Handlers.Add(new FileLogHandler(context.Id + ".log"));
+
+                // driver is an IWebDriver instance, baseUri is the base URL of the app being tested.
+
+                for (int i = 0; i < 99; i++)
                 {
                     await driver.Navigate().GoToUrlAsync(new Uri(baseUri, "blog/post-1"));
                     // Writing to file to check the contents and to make sure that the page source is indeed fully loaded.
@@ -34,6 +41,9 @@ public class PerformanceTests : UITestBase
 
                     _testOutputHelper.WriteLine($"Iteration {i + 1} completed.");
                 }
+
+                File.Copy(context.Id + ".log", context.Id + "2.log");
+                context.AppendTestDump(context.Id + "2.log");
             },
             configuration =>
             {
