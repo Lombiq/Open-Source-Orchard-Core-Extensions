52d3c1c4105599743eea576e4eab0872b4385a66
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-06-27 18:24:11 +0200

    Using Selenium.WebDriver to 4.31.0, adding perf test

---
+++ test/Lombiq.OSOCE.Tests.UI/Tests/UITestingToolboxTests/PerformanceTests.cs
@@ -0,0 +1,27 @@
+using Lombiq.Tests.UI.Extensions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Lombiq.OSOCE.Tests.UI.Tests.UITestingToolboxTests;
+
+public class PerformanceTests : UITestBase
+{
+    public PerformanceTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public Task TestSeleniumPerformance() =>
+        ExecuteTestAfterSetupAsync(
+            async context =>
+            {
+                for (int i = 0; i < 99; i++)
+                {
+                    await context.GoToRelativeUrlAsync("/blog/post-1");
+                    await context.GoToRelativeUrlAsync("/about");
+                    await context.GoToHomePageAsync();
+                    _testOutputHelper.WriteLine($"Iteration {i + 1} completed.");
+                }
+            });
+}
