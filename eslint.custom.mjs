065c5def0a81c2ce66ed1d84254412c609cc654c
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-01-21 12:58:42 +0100

    Ignore copied file

--- eslint.custom.mjs
+++ eslint.custom.mjs
@@ -5,6 +5,8 @@ export default [
             'src/Modules/Lombiq.VueJs/Lombiq.VueJs/**',
             'test/Lombiq.UITestingToolbox/Lombiq.Tests.UI.Samples/Tests/JavaScriptTests.mjs',
             'test/Lombiq.UITestingToolbox/Lombiq.Tests.UI/ui-testing-toolkit.mjs',
+            // Ignore copied file.
+            'src/Modules/Lombiq.HelpfulExtensions/Lombiq.HelpfulExtensions/wwwroot/js/trumbowyg.highlight.extension.js',
         ],
     },
 ];
