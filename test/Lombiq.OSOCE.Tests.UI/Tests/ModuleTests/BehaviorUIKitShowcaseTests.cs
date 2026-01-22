0df0f6018e3cd103c612585f939195c52e9c8db8
Sára El-Saig <sara.el-saig@lombiq.com>
2026-01-22 16:50:43 +0100

    Upgrade OC preview.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitShowcaseTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitShowcaseTests.cs
@@ -17,7 +17,5 @@ public class BehaviorUIKitShowcaseTests : UITestBase
             context => context.TestUIKitShowcaseBehaviorAsync(),
             configuration => configuration.HtmlValidationConfiguration
                 .WithRelativeConfigPath("NoUniqueLandmark.htmlvalidate.json")
-                .WithOC15222Filter()
-                // This is necessary until https://github.com/OrchardCMS/OrchardCore/pull/18730 is merged.
-                .WithFilters("OC-18730", error => error.RuleId is not "aria-label-misuse"));
+                .WithOC15222Filter());
 }
