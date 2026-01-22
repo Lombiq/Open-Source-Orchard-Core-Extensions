a6fc8637e0e07b0bc321515c99d6f201a490832b
Sára El-Saig <sara.el-saig@lombiq.com>
2026-01-22 01:26:22 +0100

    Since https://github.com/OrchardCMS/OrchardCore/issues/18510 is resolved.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitShowcaseTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorUIKitShowcaseTests.cs
@@ -23,15 +23,11 @@ public class BehaviorUIKitShowcaseTests : UITestBase
                 {
                     configuration.HtmlValidationConfiguration.WithRelativeConfigPath("NoUniqueLandmark.htmlvalidate.json");
 
-                    // The first three rule exclusions due to https://github.com/OrchardCMS/OrchardCore/issues/15222,
-                    // can be removed once it is resolved.
-                    // "aria-label-misuse" due to https://github.com/OrchardCMS/OrchardCore/issues/18510.
+                    // Rule exclusions due to https://github.com/OrchardCMS/OrchardCore/issues/15222, can be removed
+                    // once it is resolved.
                     var errors = validationResult.GetParsedErrors()
                         .Where(error =>
-                            error.RuleId is not "prefer-native-element" and
-                                not "text-content" and
-                                not "no-redundant-role" and
-                                not "aria-label-misuse")
+                            error.RuleId is not ("prefer-native-element" or "text-content" or "no-redundant-role"))
                         .ToList();
                     errors.ShouldBeEmpty(HtmlValidationResultExtensions.GetParsedErrorMessageString(errors));
                     return Task.CompletedTask;
