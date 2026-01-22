8087b58f2d41a9e974d161bd6ec2ef2adaf0281e
Krisztián Németh <nkrisz5@gmail.com>
2026-01-22 11:12:34 +0100

    Update eslint.custom.mjs
    Co-authored-by: Benedek Farkas <benedek.farkas@lombiq.com>

--- eslint.custom.mjs
+++ eslint.custom.mjs
@@ -5,7 +5,7 @@ export default [
             'src/Modules/Lombiq.VueJs/Lombiq.VueJs/**',
             'test/Lombiq.UITestingToolbox/Lombiq.Tests.UI.Samples/Tests/JavaScriptTests.mjs',
             'test/Lombiq.UITestingToolbox/Lombiq.Tests.UI/ui-testing-toolkit.mjs',
-            // Ignore copied file.
+            // Ignore the modded copy of a Trumbowyg script.
             'src/Modules/Lombiq.HelpfulExtensions/Lombiq.HelpfulExtensions/wwwroot/js/trumbowyg.highlight.extension.js',
         ],
     },
