524df364262d21ce7799e4f0325494d25efdcc41
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-03-06 16:58:05 +0100

    Add short url part

---
+++ Lombiq.Marketing.UrlShortener/Models/ShortUrlPart.cs
@@ -0,0 +1,10 @@
+﻿using OrchardCore.ContentFields.Fields;
+using OrchardCore.ContentManagement;
+
+namespace Lombiq.Marketing.UrlShortener.Models;
+
+public class ShortUrlPart : ContentPart
+{
+    public TextField ShortUrl { get; set; } = new();
+    public TextField DestinationUrl { get; set; } = new();
+}
