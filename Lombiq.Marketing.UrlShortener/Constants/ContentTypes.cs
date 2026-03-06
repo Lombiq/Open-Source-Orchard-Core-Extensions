5cf33b3606d03427aa6bebd4e3a8791cb2503a1c
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-03-06 16:58:52 +0100

    Add constants

---
+++ Lombiq.Marketing.UrlShortener/Constants/ContentTypes.cs
@@ -0,0 +1,6 @@
+﻿namespace Lombiq.Marketing.UrlShortener.Constants;
+
+public static class ContentTypes
+{
+    public const string ShortUrl = nameof(ShortUrl);
+}
---
+++ Lombiq.Marketing.UrlShortener/Constants/FeatureIds.cs
@@ -0,0 +1,6 @@
+namespace Lombiq.Marketing.UrlShortener.Constants;
+
+public static class FeatureIds
+{
+    public const string UrlShortener = "Lombiq.Marketing.UrlShortener";
+}
