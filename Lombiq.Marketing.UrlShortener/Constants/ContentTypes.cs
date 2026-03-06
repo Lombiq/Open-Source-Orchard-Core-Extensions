44c9321e9594ce3cf437b3816a6525d4f0351f40
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-03-06 18:08:41 +0100

    Move already implemented logic to new repository

--- Lombiq.Marketing.UrlShortener/Constants/ContentTypes.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Lombiq.Marketing.UrlShortener.Constants;
-
-public static class ContentTypes
-{
-    public const string ShortUrl = nameof(ShortUrl);
-}
--- Lombiq.Marketing.UrlShortener/Constants/FeatureIds.cs
+++
@@ -1,6 +0,0 @@
-namespace Lombiq.Marketing.UrlShortener.Constants;
-
-public static class FeatureIds
-{
-    public const string UrlShortener = "Lombiq.Marketing.UrlShortener";
-}
