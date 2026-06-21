4dc12b18cfd7b72886cb2ac67fe589ec9c1c5bb8
Sára El-Saig <sara.el-saig@lombiq.com>
2026-06-21 02:03:11 +0200

    Add WalkthroughsShouldWorkCorrectly workaround.

--- test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
+++ test/Lombiq.OSOCE.Tests.UI/Tests/ModuleTests/BehaviorWalkthroughsTests.cs
@@ -1,3 +1,4 @@
+using Lombiq.Tests.UI.Extensions;
 using Lombiq.Walkthroughs.Tests.UI.Extensions;
 using System.Threading.Tasks;
 using Xunit;
@@ -15,7 +16,14 @@ public class BehaviorWalkthroughsTests : UITestBase
     public Task WalkthroughsShouldWorkCorrectly() =>
         ExecuteTestAsync(
             context => context.RunSetupAndTestWalkthroughsBehaviorAsync(),
-            changeConfiguration: configuration => configuration
-                .HtmlValidationConfiguration
-                .WithRelativeConfigPath("WalkthroughsShouldWorkCorrectly.htmlvalidate.json"));
+            changeConfiguration: configuration =>
+            {
+                configuration
+                    .HtmlValidationConfiguration
+                    .WithRelativeConfigPath("WalkthroughsShouldWorkCorrectly.htmlvalidate.json");
+
+                // The preview page sends these requests prematurely, which fails validation. See
+                // https://github.com/OrchardCMS/OrchardCore/pull/19376#issuecomment-4760348280.
+                configuration.WithIgnoreExpectedStatusResponseFilter("/Preview/Draft", 500);
+            });
 }
